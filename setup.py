ff34c5901287115196a837f2f8ce1fb6ad97daed
chuanwu.sun <chuanwu.sun@ele.me>
2017-04-10 02:33:28 +0800

    upload and register pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 entry_points = [
     # todo
     "todos = PyTodos.todo:todos"
 ]
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
 setup(
     name="pytodos",
-    version='0.0.1',
+    version='1.0.1',
     description="",
-    long_description=long_description,
+    long_description="",
     author="chuanwu",
     author_email="chuanwusun@gmail.com",
-    packages=find_packages(),
+    packages=['pytodos'],
     url="https://github.com/chuanwu/PyToDos.py",
     entry_points={"console_scripts": entry_points},
     install_requires=[],
